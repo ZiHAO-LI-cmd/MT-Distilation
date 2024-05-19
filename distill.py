f37c2cd39549c887a1194d056f161124b566ea16
zihao <zihao-lee@outlook.com>
2024-05-20 01:03:48 +0800

    update

--- distill.py
+++ distill.py
@@ -6,18 +6,16 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from torch.nn.functional import log_softmax, softmax
 from torch.utils.data import Dataset, DataLoader
-
-
-def read_text_file(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        lines = [line.strip() for line in file.readlines()]
-    return lines
+import wandb
+import argparse
 
 
 class TranslationDataset(Dataset):
-    def __init__(self, src_sentences, tgt_sentences, tokenizer, max_length=512):
+    def __init__(
+        self, src_sentences, tgt_sentences, tgt_lang, tokenizer, max_length=512
+    ):
         self.tokenizer = tokenizer
-        self.src_sentences = [" >>ukr<< " + sent for sent in src_sentences]
+        self.src_sentences = [f" >>{tgt_lang}<< " + sent for sent in src_sentences]
         self.tgt_sentences = tgt_sentences
         self.max_length = max_length
 
@@ -55,7 +53,19 @@ class TranslationDataset(Dataset):
         return model_inputs
 
 
-def calculate_loss(student_outputs, teacher_outputs, labels):
+def read_text_file(file_path):
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            lines = [line.strip() for line in file.readlines()]
+        return lines
+    except IOError as e:
+        print(f"Error reading file {file_path}: {e}")
+        return []
+
+
+def calculate_loss(
+    student_outputs, teacher_outputs, labels, temperature, alpha, batch_size
+):
     s_logits = student_outputs.logits
     t_logits = teacher_outputs.logits
 
@@ -75,45 +85,152 @@ def calculate_loss(student_outputs, teacher_outputs, labels):
     return loss
 
 
-os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+def validate(model, dataloader, device):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for batch in dataloader:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            teacher_outputs = teacher_model(
+                input_ids=batch["input_ids"],
+                attention_mask=batch.get("attention_mask"),
+                decoder_input_ids=batch["decoder_input_ids"],
+            )
+            student_outputs = model(
+                input_ids=batch["input_ids"],
+                attention_mask=batch.get("attention_mask"),
+                decoder_input_ids=batch["decoder_input_ids"],
+            )
+            loss = calculate_loss(
+                student_outputs,
+                teacher_outputs,
+                batch["labels"],
+                temperature,
+                alpha,
+                batch_size,
+            )
+            total_loss += loss.item()
+    avg_loss = total_loss / len(dataloader)
+    return avg_loss
+
+
+parser = argparse.ArgumentParser(
+    description="Train a machine translation model with distillation."
+)
+parser.add_argument(
+    "--learning_rate",
+    type=float,
+    default=0.001,
+    help="Learning rate for the optimizer.",
+)
+parser.add_argument(
+    "--batch_size", type=int, default=4, help="Batch size for training and validation."
+)
+parser.add_argument(
+    "--num_epochs", type=int, default=3, help="Number of epochs to train."
+)
+parser.add_argument(
+    "--temperature", type=float, default=5.0, help="Temperature for distillation."
+)
+parser.add_argument(
+    "--alpha", type=float, default=0.5, help="Weight for the distillation loss."
+)
+parser.add_argument(
+    "--src_file_path",
+    type=str,
+    required=True,
+    help="Source file path for training data.",
+)
+parser.add_argument(
+    "--tgt_file_path",
+    type=str,
+    required=True,
+    help="Target file path for training data.",
+)
+parser.add_argument("--tgt_lang", type=str, required=True, help="Target language code.")
+parser.add_argument("--model_name", type=str, required=True, help="Teacher model name.")
+parser.add_argument(
+    "--model_save_path",
+    type=str,
+    default="./models",
+    help="Path to save the trained model.",
+)
+parser.add_argument(
+    "--wandb_project",
+    type=str,
+    default="translation-distillation",
+    help="Weights & Biases project name.",
+)
+args = parser.parse_args()
+
+learning_rate = args.learning_rate
+batch_size = args.batch_size
+num_epochs = args.num_epochs
+temperature = args.temperature
+alpha = args.alpha
+src_file_path = args.src_file_path
+tgt_file_path = args.tgt_file_path
+tgt_lang = args.tgt_lang
+model_save_path = args.model_save_path
+model_name = args.model_name
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_name = "Helsinki-NLP/opus-mt-en-mul"
 teacher_model = MarianMTModel.from_pretrained(model_name)
 teacher_model.to(device)
 teacher_model.eval()
-
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 
-uk_file_path = "./data/en-uk/NLLB.en-uk.uk"
-en_file_path = "./data/en-uk/NLLB.en-uk.en"
+src_sentences = read_text_file(src_file_path)
+tgt_sentences = read_text_file(tgt_file_path)
 
-uk_sentences = read_text_file(uk_file_path)
-en_sentences = read_text_file(en_file_path)
-
-assert len(uk_sentences) == len(
-    en_sentences
+assert len(src_sentences) == len(
+    tgt_sentences
 ), "The number of sentences must be the same in both files."
 
-train_dataset = TranslationDataset(en_sentences, uk_sentences, tokenizer)
+split_ratio = 0.9
+split_index = int(len(src_sentences) * split_ratio)
+
+# Training Set
+train_src_sentences = src_sentences[:split_index]
+train_tgt_sentences = tgt_sentences[:split_index]
+train_dataset = TranslationDataset(
+    train_src_sentences, train_tgt_sentences, tgt_lang, tokenizer
+)
+
+# Validation Set
+val_src_sentences = src_sentences[split_index:]
+val_tgt_sentences = tgt_sentences[split_index:]
+val_dataset = TranslationDataset(
+    val_src_sentences, val_tgt_sentences, tgt_lang, tokenizer
+)
 
 student_config = teacher_model.config
 student_model = MarianMTModel(student_config)
 student_model.to(device)
 
-learning_rate = 0.001
-batch_size = 4
-num_epochs = 3
-temperature = 5
-alpha = 0.5
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 optimizer = torch.optim.AdamW(student_model.parameters(), lr=learning_rate)
 
+
+run = wandb.init(
+    project=args.wandb_project,
+    config={
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "num_epochs": num_epochs,
+        "temperature": temperature,
+        "alpha": alpha,
+    },
+)
+
+best_loss = float("inf")
 for epoch in range(num_epochs):
     student_model.train()
-    total_loss = 0
+    total_train_loss = 0
 
-    for batch in tqdm(train_loader, desc=f"Epoch {epoch}"):
+    for batch in tqdm(train_loader, desc=f"Training Epoch {epoch}"):
         optimizer.zero_grad()
 
         batch = {k: v.to(device) for k, v in batch.items()}
@@ -129,11 +246,30 @@ for epoch in range(num_epochs):
             decoder_input_ids=batch["decoder_input_ids"],
         )
 
-        loss = calculate_loss(student_outputs, teacher_outputs, batch["labels"])
+        loss = calculate_loss(
+            student_outputs,
+            teacher_outputs,
+            batch["labels"],
+            temperature,
+            alpha,
+            batch_size,
+        )
         loss.backward()
         optimizer.step()
-        total_loss += loss.item()
+        total_train_loss += loss.item()
 
-    print(f"Average loss: {total_loss / len(train_loader)}")
+    avg_train_loss = total_train_loss / len(train_loader)
+    avg_val_loss = validate(student_model, val_loader, device)
+    wandb.log({"Training Loss": avg_train_loss, "Validation Loss": avg_val_loss})
 
-student_model.save_pretrained("distilled-opus-mt-translation-model")
+    # Keep best checkpoint
+    if avg_val_loss < best_loss:
+        best_loss = avg_val_loss
+        model_save_dir = os.path.join(model_save_path, "best.pt")
+        student_model.save_pretrained(model_save_dir)
+        print(f"Saved best model checkpoint at {model_save_dir}")
+
+model_save_dir = os.path.join(model_save_path, "last.pt")
+student_model.save_pretrained(model_save_dir)
+print(f"Saved last model at {model_save_dir}")
+wandb.finish()
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python train_translation_model.py \
+  --learning_rate 0.001 \
+  --batch_size 4 \
+  --num_epochs 3 \
+  --temperature 5.0 \
+  --alpha 0.5 \
+  --src_file_path "./data/en-uk/NLLB.en-uk.en" \
+  --tgt_file_path "./data/en-uk/NLLB.en-uk.uk" \
+  --tgt_lang "ukr" \
+  --model_name "Helsinki-NLP/opus-mt-tc-big-en-zle" \
+  --model_save_path "./models" \
+  --wandb_project "MT-Distilation"
